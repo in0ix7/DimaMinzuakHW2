5c2036febd7f74374751cb82a03a22aa90f9621d
Ilya Nilov <nilov2543@gmail.com>
2020-05-03 17:37:50 +0300

    5-6 неделя

--- DimaMinzuakHW2/Source.cpp
+++ DimaMinzuakHW2/Source.cpp
@@ -1,124 +1,41 @@
 ﻿#include <iostream>
-# include < математика.h>
+# include < math.h>
 int main()
 {
-	//Çàäàíèå 1. Âââñòè íàïêðàí ñòðîê èç íóëåé, âðè÷åìêîëè÷åñòâî íóëåé âèææäîé ñòðîå ðàâíî íîìåðó ñòðîè.
-	// Éîëè÷åñòâî ñòðîê Í ââîäèòîëüçîâàòåëü (îæíî èç ôàéà).
-	int Zadanie1;
-	std::cin >> > Zadanie1;
-	для(int i = 1; i < Zadanie1 + 1; i++) {
-		для(int j = 0; j)
-			std::cout << 0;
-		std::cout < ;
-	}
-	// Çàäàíèå 2i. Äðîòàóëèðîâàòü ôóíêèþ (øàã è äèàïçîí çàäàòîëüçîâàòåëü):
-	поплавок dx, xs, xf;
-	const  float eps = 0.000001; / / âñ, ÷ñî âíüøå âòîãî ÷èñëà ñ÷èòàåì íóëì
-
-		std::cout < < " Enter [xs, xf]: ";
-	std::cin > > > xs > > > > > > > > xf;
-
-	std::cout < < " Enter dx: ";
-	std::cin >> > dx;
-
-	// çàãîëîâîêñàáëèö
-	std::cout < < "\t x \t\t y" < ;
-
-	std::cout.точность(5);
-	поплавок f;
-	поплавок x = xs; / / íà÷èíàåì ñ÷èòàòü ñåâîãî éðàÿ èíòåðâà
-		в то время как(x) {
-		если(fabs(x - 2))
-			std::cout < < " \t"
-			<< икс
-			< < "\t \ T делим на ноль"
-			< ;
-		остальное{
-  f = -x * (3 + 2 * x) * (3 + 2 * x) + 3 * x + sin(2 * x);
-
-  std::cout < < " \t"
-  << икс
-  < < "\t\t"
-  << Ф
-  < ;
-		}
-		x += dx;
-	}
-
-	// Çàäàíèå 2o. Äðîòàóëèðîâàòü ôóíêèþ (øàã è äèàïçîí çàäàòîëüçîâàòåëü):
-	std::cout < < " Enter [xs, xf]: ";
-	std::cin > > > xs > > > > > > > > xf;
-
-	std::cout < < " Enter dx: ";
-	std::cin >> > dx;
-
-	// çàãîëîâîêñàáëèö
-	std::cout << "\tx\t\ty" << std::endl;
-
-	std::cout.precision(5);
-	x = xs;  // íà÷èíàåì ñ÷èòàòü ñ ëåâîãî êðàÿ èíòåðâàëà
-	while (x < xf) {
-		if (fabs(x - 2) < eps)
-			std::cout << "\t"
-			<< x
-			<< "\t\t Divide on zero"
-			<< std::endl;
-		else {
-			f = sqrt(15 + 4 / x) + 13 * x + sin(3 * x + 13);
-
-			std::cout << "\t"
-				<< x
-				<< "\t\t"
-				<< f
-				<< std::endl;
-		}
-		x += dx;
-	}
-	//Çàäàíèå 4i. Äàíà ôóíêöèÿ y=f(x). Íàéòè çíà÷åíèå ôóíêöèè ïî x:
-	std::cout << "X";
-	std::cin >> x;
-	if (x > 7) { std::cout << "y = " << 2 * (x * x) - 3 << std::endl; }
-	else { std::cout << "X !< 7" << std::endl; }
-	std::cout << "y = " << 0 << std::endl;
-	std::cout << "X";
-	std::cin >> x;
-	if (x < 7) { std::cout << "y = " << 2 * abs(x) + 3 << std::endl; }
-	else { std::cout << " X !> 7>" < ; }
-
-	//Çàäàíèå 5. Âââñòè íàïêðàí ðÿä íàòóðàëüíõ ÷èñåë îò Í äî Ñ øàãîì Ø. 
-	// Íàïðèìåð, åñëè Í 10, ø 35, Ø 5, âîâîä äîëæåí áûòü ñàêèì: 10 15 20 25 30 35. 
-	// Çíà÷åíèÿ Í, Ì, Ø óêàçüâàòòñÿ âîëüçîâàòåëåì (ñ÷èòûâàòòñÿ ñêàâèàòóðû).
-	int xmin, xmax, shag;
-	std::cout < < "xmin, xmax, shag" < ;
-	std::cin > > > xmin > > > > > > > xmax >>>>>>>>>> > махорка;
-	в то время как(xmin) {
-		std::cout < < xmin << " ";
-		xmin += shag;
-	}
-	std::cout < ;
-	// Çàäàíèå 3i. Âîëüçîâàòåëü çàäàò ÷èñëî × è. Îîñ÷èòàòü y:
-	int chislo, kolichestvo;
-	количество = 0;
-	std::cout < < chislo, shag ";
-		std::cin > > > chislo > > > > > > > > колічество; > > > > / / ×èñëî
-		чисо + 1;
-	для(int i = 0; i)
+	int arr[10]; //Массив
+	int Exercise2i = 0; //Сумма меньше 4
+	int Exercise2c = 0; //Сумма больше 3
+	int Exercise6max = 0; int Exercise6indexmax = 0; //Максимальное чило в массиве
+	int Exercise6min = 10; int Exercise6indexmin = 0; //Минимальное чило в массиве
+	int Exercise5max = 0; int Exercise5indexmax = 0; //Номер максимального числа
+	int Exercise5min = 10; int Exercise5indexmin = 0;//Номер минимального числа
+	int Zadannoechislo = 0; int Kollichestvo = 0; std::cin >> Zadannoechislo; // Ввод заданного числа для задания 10
+	int Exercise14 = 0; //Задание 14
+	for (int i = 0; i < 10; i++)
 	{
-		число + = -pow(число, количество) / kolichestvo;
-	}
-	std::cout << "y =" < < chislo < ;
+		arr[i] = rand() % 10; //Задание 1. Заполнить массив случайными чисами в диапазоне от 0 до 10.
+			  //Вычислить сумму элементов массива,
+		if (arr[i] < 4) { Exercise2i += arr[i]; }; //меньших 4.
+		if (arr[i] > 3) { Exercise2c += arr[i]; }; //больших 3.
+		//Задание 6. Найти номер минимального/максимального по модулю элемента
+		if (abs(arr[i]) > Exercise6max) { Exercise6max = arr[i]; Exercise6indexmax = i; };
+		if (abs(arr[i]) < Exercise6min) { Exercise6min = arr[i]; Exercise6indexmin = i; };
+		//Задание 5. Найти наибольший/наименьший элемент массива и его номер по порядку
+		if (arr[i] > Exercise5max) { Exercise5max = arr[i]; Exercise5indexmax = i; }; //Наибольший элемент массива и его номер
+		if (arr[i] < Exercise5min) { Exercise5min = arr[i]; Exercise5indexmin = i; }; //Наименьший элемент массива и его номер
+		//Задание 10. Посчитать количество (и вывести на экран) элементов массива, больших заданного числа
+		if (arr[i] > Zadannoechislo) { Kollichestvo++; };
+		//Задание 12. Увеличить все нечётные элементы массива на 3, а чётные в 2 раза
+		if (arr[i] % 2 == 0) { arr[i] = arr[i] * 2; }
+		else { arr[i] += 3; }
+		//Задание 14. Найти наибольший элемент массива среди элементов с чётными индексами.
+		if (i % 2 == 0) { if (arr[i] > Exercise14) { Exercise14 = arr[i]; } }
 
-	//Çàäàíèå 3ñ.
-	количество = 0;
-	std::cout < < chislo, shag ";
-		std::cin > > > chislo > > > > > > > > колічество; > > > > / / ×èñëî
-		для(int i = 0; i)
-	{
-		число += 1 / число * число;
 	}
-	std::cout << "y =" < < chislo < ;
+	std::cout << Kollichestvo << std::endl;
+
 
 	std::getchar();
 	std::getchar();
-	возврат  0;
+	return 0;
 }
